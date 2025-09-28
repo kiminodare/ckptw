496faa94a5f34277ad6713668f53cf181f04ba27
kiminodare <alderamemi@gmail.com>
2025-09-28 23:04:40 +0700

    fix image

--- utils/proofHelpers.ts
+++ utils/proofHelpers.ts
@@ -11,12 +11,21 @@ export function resolveCurrentProofUrl(
 ): string | null {
     const latestAppeal = proof.appeals.length > 0 ? proof.appeals[0] : null
 
-    if (type === 'VIP') {
-        return latestAppeal?.newProofVipURL ?? null
+    if (latestAppeal) {
+        if (type === 'VIP') {
+            return latestAppeal.newProofVipURL ?? null
+        }
+        if (type === 'SUMMIT') {
+            return latestAppeal.newProofSummitURL ?? null
+        }
+        return null
     }
 
+    if (type === 'VIP') {
+        return proof.proofURLVip ?? null
+    }
     if (type === 'SUMMIT') {
-        return latestAppeal?.newProofSummitURL  ?? null
+        return proof.proofURLSummit ?? null
     }
 
     return null
